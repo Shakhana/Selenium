d3568a99b4f77e16cdf60b597e49f991149bb58e
Shakhana <shamilova.08@gmail.com>
2022-11-27 23:45:13 +0300

    Initial commit

---
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -0,0 +1,15 @@
+package ru.netology.web;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+
+public class CardOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+System.setProperty("web")
+    }
+
+
+}
